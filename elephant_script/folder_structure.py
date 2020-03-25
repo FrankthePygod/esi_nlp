40e4807e0d738c48fdc3a161c25d82d485089868
frank_meng <frank.meng@annalect.com>
2020-03-25 00:41:13 -0500

    commit several test files

---
+++ elephant_script/folder_structure.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+
+def create_dir(test = False):
+
+path = os.getcwd()
+path
+
+
+if __name__ = '__main__':
+    y = input('is this a test run? (Y/n)')
+    if y == 'Y':
+        create_dir(test = True)
+    else:
+        create_dir()
