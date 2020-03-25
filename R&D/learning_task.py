805116ee910dc75be02b35faa7106cc605ad0cf8
Oliviaouweiyi <ouweiyi95@hotmail.com>
2020-03-25 00:45:18 -0500

    add a learning script

---
+++ R&D/learning_task.py
@@ -0,0 +1,14 @@
+import sqlite3
+import sqlalchemy
+
+#research sqlalchemy
+
+#find a good dummy dataset:(suggetions: Titanic3/Chicago Crime/Census)
+
+#create your first schema and table
+
+#convert output to pandas datafram
+
+#plot the output
+
+#change atom syntax color
